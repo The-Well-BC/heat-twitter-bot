1289c9bf34c5670f9d8915c0e3a5cc3f465360ec
Adesuwa T. Dawodu <littlezigy@gmail.com>
2021-02-12 12:42:21 +0100

    Update - increase frequency of cron job

--- components/cronJobs.js
+++ components/cronJobs.js
@@ -3,8 +3,8 @@ const heatcheckPayouts = require('./twitter/heatcheckPayouts');
 
 module.exports = () => {
     if(process.env.NODE_ENV !== 'test') {
-        // Ten Minutes
-        cron.schedule('*/10 * * * *', function() {
+        // Five Minutes
+        cron.schedule('*/5 * * * *', function() {
             console.log('Making HEAT payouts');
             return heatcheckPayouts();
         });
